be4184c8071d2a60a21bde519ac3a63997bf0589
Joel Miller <millerjoel7597@gmail.com>
2026-04-20 14:07:44 +0100

    feat: integrate InvoiceForm component and manage form state in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import Invoices from './components/Invoices'
+import InvoiceForm from './components/InvoiceForm'
 
 const App = () => {
+  const [isFormOpen, setIsFormOpen] = useState(false)
+
   return (
     <div className="min-h-screen bg-background font-league">
       <Header />
       <div className="lg:pl-[103px]">
-        <Invoices />
+        <Invoices onOpenForm={() => setIsFormOpen(true)} />
       </div>
+      <InvoiceForm isOpen={isFormOpen} onClose={() => setIsFormOpen(false)} />
     </div>
   )
 }
+
 
 export default App
