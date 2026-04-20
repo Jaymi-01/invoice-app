8be3adc24f10942a78284c6591fcaf72bb5c2952
Joel Miller <millerjoel7597@gmail.com>
2026-04-20 13:07:16 +0100

    feat: enhance App component layout with Invoices integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
+import Invoices from './components/Invoices'
 
 const App = () => {
   return (
-    <div>
+    <div className="min-h-screen bg-background font-league">
       <Header />
+      <div className="lg:pl-[103px]">
+        <Invoices />
+      </div>
     </div>
   )
 }
