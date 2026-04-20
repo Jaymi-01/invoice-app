116a8472072d90b9e4e27af2a4d7de6fc7ddb26e
Joel Miller <millerjoel7597@gmail.com>
2026-04-20 16:26:05 +0100

    feat: add Item and Invoice interfaces to define data structures

---
+++ src/types.ts
@@ -0,0 +1,32 @@
+export interface Item {
+  id: string
+  name: string
+  quantity: number
+  price: number
+  total: number
+}
+
+export interface Invoice {
+  id: string
+  createdAt: string
+  paymentDue: string
+  description: string
+  paymentTerms: number
+  clientName: string
+  clientEmail: string
+  status: 'paid' | 'pending' | 'draft'
+  senderAddress: {
+    street: string
+    city: string
+    postCode: string
+    country: string
+  }
+  clientAddress: {
+    street: string
+    city: string
+    postCode: string
+    country: string
+  }
+  items: Item[]
+  total: number
+}
