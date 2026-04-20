3273113652422ac69701f13137d599a8dedf55c1
Joel Miller <millerjoel7597@gmail.com>
2026-04-20 16:25:34 +0100

    feat: manage invoices state and integrate addInvoice functionality in App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,34 @@ import React, { useState } from 'react'
 import Header from './components/Header'
 import Invoices from './components/Invoices'
 import InvoiceForm from './components/InvoiceForm'
+import type { Invoice } from './types'
 
 const App = () => {
   const [isFormOpen, setIsFormOpen] = useState(false)
+  const [invoices, setInvoices] = useState<Invoice[]>([])
+
+  const addInvoice = (invoice: Invoice) => {
+    setInvoices([invoice, ...invoices])
+  }
 
   return (
     <div className="min-h-screen bg-background font-league">
       <Header />
       <div className="lg:pl-[103px]">
-        <Invoices onOpenForm={() => setIsFormOpen(true)} />
+        <Invoices 
+          invoices={invoices} 
+          onOpenForm={() => setIsFormOpen(true)} 
+        />
       </div>
-      <InvoiceForm isOpen={isFormOpen} onClose={() => setIsFormOpen(false)} />
+      <InvoiceForm 
+        isOpen={isFormOpen} 
+        onClose={() => setIsFormOpen(false)} 
+        onAddInvoice={addInvoice}
+      />
     </div>
   )
 }
+
 
 
 export default App
